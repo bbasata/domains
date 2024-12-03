46fdebfb6fab603068497fea00460bbabffd599c
Baraa Basata <bbasata@gmail.com>
2024-12-03 11:19:37 -0500

    Initial commit

---
+++ Makefile
@@ -0,0 +1,20 @@
+.PHONY: terraform init plan clean apply destroy
+
+terraform: plan apply
+
+init: clean
+	terraform init
+
+plan: .terraform/plan
+
+.terraform/plan:
+	envchain dnsimple terraform plan -input -out .terraform/plan
+
+clean:
+	rm .terraform/plan
+
+apply:
+	envchain dnsimple terraform apply -auto-approve .terraform/plan
+
+destroy: clean
+	envchain dnsimple terraform destroy -auto-approve
---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    dnsimple = {
+      source  = "dnsimple/dnsimple"
+      version = "~> 1.8.0"
+    }
+  }
+}
+
+provider "dnsimple" {
+}
+
+variable "domain_name" {
+  description = "The domain name to manage in DNSimple"
+  type        = string
+}
+
+variable "public_ipv4_address" {
+  description = "The public IPv4 address to use in DNS records"
+  type        = string
+}
+
+resource "dnsimple_domain" "example" {
+  name = "${var.domain_name}"
+}
+
+resource "dnsimple_zone_record" "root" {
+  zone_name = "${dnsimple_domain.example.name}"
+  name = ""
+  value = "${var.public_ipv4_address}"
+  type = "A"
+  ttl = 3600
+}
+
+resource "dnsimple_zone_record" "www" {
+  zone_name = "${dnsimple_domain.example.name}"
+  name = "www"
+  value = "${var.public_ipv4_address}"
+  type = "A"
+  ttl = 3600
+}
